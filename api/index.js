91d03ff6cb3a8b034971fda77bc1c016236aae4c
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-15 17:54:37 +0000

    feat: add first api version

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+  return res.json({ success: true })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Server started and listening to port 3000')
+})
