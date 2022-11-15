952ffee03ff62aeb86e2ff8c2b05f820348a1ae6
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-15 18:21:16 +0000

    feat: change app component to grab data from api

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/api')
+      .then(({ data }) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error(error)
+        setMessage(error.response.data)
+      })
+  }, [message])
+
   return (
     <div className="App">
       <header className="App-header">
+        <h2>{message}</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
