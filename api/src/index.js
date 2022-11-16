3423e4faa78df4c00ffc75a3a433c3c84d849b30
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-16 09:48:52 +0000

    feat: make knex migrations and seeds run at app startup

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,22 @@
+const environment = process.env.NODE_ENV || 'development'
 const express = require('express')
 const app = express()
+const knex = require('./database')
+knex.migrate.latest()
+  .then(() => {
+    if (environment === 'development') {
+      knex.seed.run()
+        .then(() => {
+          console.log('Seed runned')
+        })
+        .catch((error) => {
+          console.error('Seed not runned ', error)
+        })
+    }
+  })
+  .catch((error) => {
+    console.error('Migration not runned ', error)
+  })
 
 app.get("/", (req, res) => {
   return res.json({ message: 'success' })
