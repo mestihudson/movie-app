30801de26e95c214927dc3ab5fdf89bd87fae468
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-18 09:46:11 +0000

    fix: add production session

--- api/src/knexfile.js
+++ api/src/knexfile.js
@@ -16,4 +16,20 @@ module.exports = {
       directory: `${__dirname}/database/seeds`,
     },
   },
+  production: {
+    client: 'pg',
+    debug: true,
+    connection: {
+      host:     process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/database/seeds`,
+    },
 }
