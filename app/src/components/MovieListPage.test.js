27e012c15bca155026771b56d8e574f0d5025f76
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-25 21:09:56 +0000

    refactor(test): reorganize to fit into smaller dev screen

--- app/src/components/MovieListPage.test.js
+++ app/src/components/MovieListPage.test.js
@@ -4,23 +4,21 @@ import MovieListPage from './MovieListPage'
 
 describe('components/MovieListPage', () => {
   it('should call retrieve movies service when page has loaded', async () => {
-    const retrieveMoviesServiceMock = jest.fn().mockImplementation(() => Promise.resolve([1]))
+    const retrieveMoviesServiceMock = jest.fn()
+      .mockImplementation(() => Promise.resolve([1]))
 
     render(<MovieListPage retrieveMoviesService={retrieveMoviesServiceMock}/>)
 
-    await waitFor(() => {
-      expect(retrieveMoviesServiceMock).toHaveBeenCalled()
-    })
+    await waitFor(() => expect(retrieveMoviesServiceMock).toHaveBeenCalled())
   })
 
   it('should populate collection when retrieve movies service ends on successful',
     async () => {
-    const retrieveMoviesServiceMock = jest.fn().mockImplementation(() => Promise.resolve([1]))
+    const retrieveMoviesServiceMock = jest.fn()
+      .mockImplementation(() => Promise.resolve([1]))
 
     render(<MovieListPage retrieveMoviesService={retrieveMoviesServiceMock}/>)
 
-    await waitFor(() => {
-      expect(screen.queryAllByTestId('movie').length).toBe(1)
-    })
+    await waitFor(() => expect(screen.queryAllByTestId('movie').length).toBe(1))
   })
 })
