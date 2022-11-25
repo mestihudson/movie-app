18845def873e585327ebc657d2373935261ccd9a
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-25 21:03:31 +0000

    feat: populate collection when retrieve movies service ends on successful

--- app/src/components/MovieListPage.js
+++ app/src/components/MovieListPage.js
@@ -1,12 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function MovieListPage({ retrieveMoviesService }) {
+  const [collection, setCollection] = useState([])
+
   useEffect(() => {
     retrieveMoviesService()
+      .then((movies) => setCollection(movies))
   }, [])
+
   return (
     <>
       MovieListPage
+      <ul>
+        {
+          collection.map((e, i) => <li key={i} data-testid='movie'>{e}</li>)
+        }
+      </ul>
     </>
   )
 }
