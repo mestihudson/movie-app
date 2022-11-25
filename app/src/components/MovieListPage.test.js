71982a07990dd0048184fa7694ff97f82c3ba6f9
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-25 21:17:46 +0000

    refactor(test): remove duplication

--- app/src/components/MovieListPage.test.js
+++ app/src/components/MovieListPage.test.js
@@ -2,22 +2,26 @@ import { render, screen, waitFor } from '@testing-library/react'
 
 import MovieListPage from './MovieListPage'
 
+const renderMovieListPage = ({
+    retrieveMoviesServiceMock = jest.fn()
+      .mockImplementation(() => Promise.resolve([1]))
+  } = {}) => {
+  render(<MovieListPage retrieveMoviesService={retrieveMoviesServiceMock}/>)
+}
+
 describe('components/MovieListPage', () => {
   it('should call retrieve movies service when page has loaded', async () => {
     const retrieveMoviesServiceMock = jest.fn()
       .mockImplementation(() => Promise.resolve([1]))
 
-    render(<MovieListPage retrieveMoviesService={retrieveMoviesServiceMock}/>)
+    renderMovieListPage({ retrieveMoviesServiceMock })
 
     await waitFor(() => expect(retrieveMoviesServiceMock).toHaveBeenCalled())
   })
 
   it('should populate collection when retrieve movies service ends on successful',
     async () => {
-    const retrieveMoviesServiceMock = jest.fn()
-      .mockImplementation(() => Promise.resolve([1]))
-
-    render(<MovieListPage retrieveMoviesService={retrieveMoviesServiceMock}/>)
+    renderMovieListPage()
 
     await waitFor(() => expect(screen.queryAllByTestId('movie').length).toBe(1))
   })
