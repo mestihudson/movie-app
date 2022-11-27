679a588fd5f6ce1c7dc819fb524c1abd1c4e204f
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 18:05:26 +0000

    feat: handler error from api

--- app/src/services/RetrieveMoviesService.js
+++ app/src/services/RetrieveMoviesService.js
@@ -1,4 +1,8 @@
 export function retrieveMoviesService() {
   return fetch('/api/movies')
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok)
+        return response.json()
+      return Promise.reject(new Error(response))
+    })
 }
