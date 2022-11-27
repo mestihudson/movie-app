6577bed59b321460d7f467835ec5911197411bd3
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 16:15:27 +0000

    chore: add config to run msw methods during integration tests lifecycle

--- app/src/setupTests.integration.js
+++ app/src/setupTests.integration.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+import { server } from './__tests__/integration/mocks/server'
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
