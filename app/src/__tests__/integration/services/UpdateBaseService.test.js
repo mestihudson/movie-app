5fb4cd6770d47f97177c711bba2ef896e4be8c98
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 18:31:49 +0000

    test(integration): should handler error api response

--- app/src/__tests__/integration/services/UpdateBaseService.test.js
+++ app/src/__tests__/integration/services/UpdateBaseService.test.js
@@ -13,3 +13,16 @@ it('should handler successful api response', async () => {
 
   await expect(updateBaseService()).resolves.toStrictEqual({})
 })
+
+it('should handler error api response', async () => {
+  server.use(
+    rest.get('/api/update', (req, res, ctx) => {
+      return res(
+        ctx.status(500),
+        ctx.json({})
+      )
+    })
+  )
+
+  await expect(updateBaseService()).rejects.toThrow()
+})
