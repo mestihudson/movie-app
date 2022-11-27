6ea5a256335dc573ed6931b7692ecc2820bc9dc5
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 18:43:55 +0000

    feat: bind components and services to app container

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,14 @@
+import MovieListPage from './components/MovieListPage'
+import { updateBaseService } from './services/UpdateBaseService'
+import { retrieveMoviesService } from './services/RetrieveMoviesService'
+
 export default function App() {
   return (
     <>
+      <MovieListPage
+        retrieveMoviesService={retrieveMoviesService}
+        updateBaseService={updateBaseService}
+      />
     </>
   )
 }
