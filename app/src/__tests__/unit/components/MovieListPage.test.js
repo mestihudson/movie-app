5a77bf94e83626ca60859b00e519122d554efdc6
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 16:04:01 +0000

    refactor(test): move unit test files to specific source subfolder

--- app/src/__tests__/unit/components/MovieListPage.test.js
+++ app/src/__tests__/unit/components/MovieListPage.test.js
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 
-import MovieListPage from './MovieListPage'
+import MovieListPage from '../../../components/MovieListPage'
 
 const renderMovieListPage = ({
     retrieveMoviesServiceMock = jest.fn()
