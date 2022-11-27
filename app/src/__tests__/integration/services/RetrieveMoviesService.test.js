9834590c32df5d656a902fe048a84566d75e762d
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-27 18:05:11 +0000

    test(integration): should handler error from api

--- app/src/__tests__/integration/services/RetrieveMoviesService.test.js
+++ app/src/__tests__/integration/services/RetrieveMoviesService.test.js
@@ -24,3 +24,25 @@ it('should retrieve movies from api', async () => {
 
   await waitFor(() => expect(screen.queryAllByTestId('movie')).toHaveLength(2))
 })
+
+it('should handler error from api', async () => {
+  server.use(
+    rest.get('/api/movies', (req, res, ctx) => {
+      return res(
+        ctx.status(500),
+        ctx.json({})
+      )
+    })
+  )
+  await waitFor(() =>
+    render(
+      <MovieListPage
+        retrieveMoviesService={retrieveMoviesService}
+      />
+    )
+  )
+
+  await waitFor(() =>
+    expect(screen.queryAllByTestId('error-alert-message')).toHaveLength(1)
+  )
+})
