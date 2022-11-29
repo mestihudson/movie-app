781361ec3519680e9d4eab4a172fe26cda973275
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-29 22:25:43 +0000

    refactor: remove duplication

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,8 +1,8 @@
 const environment = process.env.NODE_ENV || 'development'
 
-const knexfile = require('../knexfile')
+const knexfile = require('../knexfile')[environment]
 
-const knex = require('knex')(knexfile[environment])
+const knex = require('knex')(knexfile)
 
 const migrate = () => {
   knex.migrate.latest()
