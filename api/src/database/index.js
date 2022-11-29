4f7bc98326f62c36bc84a1a50fe18a3806660260
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-29 19:01:47 +0000

    refactor: move migration logic to specific point

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,5 +1,25 @@
 const environment = process.env.NODE_ENV || 'development'
+
 const knexfile = require('../knexfile')
+
 const knex = require('knex')(knexfile[environment])
 
-module.exports = knex
+const migrate = () => {
+  knex.migrate.latest()
+    .then(() => {
+      if (environment === 'development') {
+        knex.seed.run()
+          .then(() => {
+            console.log('Seed runned')
+          })
+          .catch((error) => {
+            console.error('Seed not runned ', error)
+          })
+      }
+    })
+    .catch((error) => {
+      console.error('Migration not runned ', error)
+    })
+}
+
+module.exports = { knex, migrate }
--- api/src/index.js
+++ api/src/index.js
@@ -1,23 +1,8 @@
 require('dotenv').config({ path: '/opt/projects/movie/api/.env' })
-const environment = process.env.NODE_ENV || 'development'
-const express = require('express')
-const app = express()
-const knex = require('./database')
-knex.migrate.latest()
-  .then(() => {
-    if (environment === 'development') {
-      knex.seed.run()
-        .then(() => {
-          console.log('Seed runned')
-        })
-        .catch((error) => {
-          console.error('Seed not runned ', error)
-        })
-    }
-  })
-  .catch((error) => {
-    console.error('Migration not runned ', error)
-  })
+
+const app = require('express')()
+
+require('./database').migrate()
 
 app.get("/", (req, res) => {
   return res.json({ message: 'success' })
