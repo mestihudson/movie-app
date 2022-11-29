1fe0716e85a6b27f9f600a249fdb66473299141c
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-29 22:24:57 +0000

    feat: add test environment group and change approach to define them

--- api/src/knexfile.js
+++ api/src/knexfile.js
@@ -1,34 +1,44 @@
-module.exports = {
-  development: {
-    client: 'pg',
-    debug: true,
+const defaultValues = {
+  client: 'pg',
+  debug: true,
+  connection: {
+    port:     process.env.DB_PORT,
+    host:     process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/database/migrations`,
+  },
+}
+
+const environments = {
+  test: {
+    ...defaultValues,
+    debug: false,
     connection: {
-      port:     process.env.DB_PORT,
-      host:     process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/database/migrations`,
+      port:     5432,
+      host:     'db',
+      database: 'postgres',
+      user:     'postgres',
+      password: 'postgres12345678',
     },
     seeds: {
       directory: `${__dirname}/database/seeds`,
     },
   },
-  production: {
-    client: 'pg',
-    connection: {
-      port:     process.env.DB_PORT,
-      host:     process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user:     process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/database/migrations`,
+  development: {
+    ...defaultValues,
+    seeds: {
+      directory: `${__dirname}/database/seeds`,
     },
   },
+  production: {
+    ...defaultValues,
+    debug: false,
+  },
 }
+
+module.exports = environments
