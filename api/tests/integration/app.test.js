dc437d0a88db1ec1fcf9b815da6bec041a7f4bc6
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-29 22:30:33 +0000

    test(integration): should return all movies from database

---
+++ api/tests/integration/app.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+
+const api = require('../../src/app')
+const { knex, migrate } = require('../../src/database')
+
+beforeAll((done) => {
+  migrate()
+    .finally(() => {
+      done()
+    })
+})
+
+afterAll((done) => {
+  knex.destroy()
+    .finally(() => {
+      done()
+    })
+})
+
+it('should return all movies from database', async () => {
+  const { status, body } = await request(api)
+    .get('/movies')
+  expect(status).toBe(200)
+  expect(body).toHaveLength(2)
+})
+
