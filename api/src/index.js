ae66e63da059c75d6194c0beb82ff5809473f438
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-11-29 22:40:33 +0000

    feat: bind entry point to app

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,7 @@
 require('dotenv').config({ path: '/opt/projects/movie/api/.env' })
 
-const app = require('express')()
-
 require('./database').migrate()
 
-app.get("/", (req, res) => {
-  return res.json({ message: 'success' })
-})
-
-app.listen(process.env.PORT, () => {
+require('./app').listen(process.env.PORT, () => {
   console.log(`Server started and listening to port ${process.env.PORT}`)
 })
