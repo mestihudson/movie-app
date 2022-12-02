e3c1ab07fcf5742aa6abb89807326ff10ff6e212
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-02 10:00:08 +0000

    fix(test): change mock to concise state

--- api/tests/integration/services/get-movies/adapters/helpers.js
+++ api/tests/integration/services/get-movies/adapters/helpers.js
@@ -5,48 +5,18 @@ const mockTmdbCall = async () => {
   nock('https://api.themoviedb.org')
     .get('/4/list/1')
     .reply(200, {
-      "page": 1,
-      "total_pages": 3,
-      "total_results": 3,
+      "total_pages": 1,
+      "total_results": 1,
       "results": [{
         id,
         "media_type": "movie",
-        "backdrop_path": "/1b.jpg",
-        "poster_path": "/1p.jpg",
-        "title": "1t",
-        "overview": "1d",
-      }]
-    })
-    .get('/4/list/1')
-    .reply(200, {
-      "page": 2,
-      "total_pages": 3,
-      "total_results": 3,
-      "results": [{
-        id,
-        "media_type": "movie",
-        "backdrop_path": "/2b.jpg",
-        "poster_path": "/2p.jpg",
-        "title": "2t",
-        "overview": "2d",
-      }]
-    })
-    .get('/4/list/1')
-    .reply(200, {
-      "page": 3,
-      "total_pages": 3,
-      "total_results": 3,
-      "results": [{
-        id,
-        "media_type": "movie",
-        "backdrop_path": "/3b.jpg",
-        "poster_path": "/3p.jpg",
-        "title": "3t",
-        "overview": "3d",
+        "backdrop_path": "/14QbnygCuTO0vl7CAFmPf1fgZfV.jpg",
+        "poster_path": "/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
+        "title": "Spider-Man: No Way Home",
+        "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
       }]
     })
     .get(`/3/movie/${id}/credits`)
-    .times(3)
     .reply(200, {
       id,
       crew: [{
--- api/tests/integration/services/get-movies/adapters/tmdb.test.js
+++ api/tests/integration/services/get-movies/adapters/tmdb.test.js
@@ -9,22 +9,58 @@ beforeEach(() => {
 })
 
 it('should retrieve data from tmdb api', async () => {
+  await mockTmdbCall()
+  const movies = await getMovies()
+  expect(movies).toHaveLength(1)
+})
+
+it('should call tmdb api such times as total pages is', async () => {
   const id = 6346649
   nock('https://api.themoviedb.org')
     .get('/4/list/1')
     .reply(200, {
-      "total_pages": 1,
-      "total_results": 1,
+      "page": 1,
+      "total_pages": 3,
+      "total_results": 3,
       "results": [{
         id,
         "media_type": "movie",
-        "backdrop_path": "/14QbnygCuTO0vl7CAFmPf1fgZfV.jpg",
-        "poster_path": "/uJYYizSuA9Y3DCs0qS4qWvHfZg4.jpg",
-        "title": "Spider-Man: No Way Home",
-        "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+        "backdrop_path": "/1b.jpg",
+        "poster_path": "/1p.jpg",
+        "title": "1t",
+        "overview": "1d",
+      }]
+    })
+    .get('/4/list/1')
+    .reply(200, {
+      "page": 2,
+      "total_pages": 3,
+      "total_results": 3,
+      "results": [{
+        id,
+        "media_type": "movie",
+        "backdrop_path": "/2b.jpg",
+        "poster_path": "/2p.jpg",
+        "title": "2t",
+        "overview": "2d",
+      }]
+    })
+    .get('/4/list/1')
+    .reply(200, {
+      "page": 3,
+      "total_pages": 3,
+      "total_results": 3,
+      "results": [{
+        id,
+        "media_type": "movie",
+        "backdrop_path": "/3b.jpg",
+        "poster_path": "/3p.jpg",
+        "title": "3t",
+        "overview": "3d",
       }]
     })
     .get(`/3/movie/${id}/credits`)
+    .times(3)
     .reply(200, {
       id,
       crew: [{
@@ -39,13 +75,6 @@ it('should retrieve data from tmdb api', async () => {
       }],
     })
   ;
-
-  const movies = await getMovies()
-  expect(movies).toHaveLength(1)
-})
-
-it('should call tmdb api such times as total pages is', async () => {
-  await mockTmdbCall()
   const movies = await getMovies()
   expect(movies).toHaveLength(3)
 })
