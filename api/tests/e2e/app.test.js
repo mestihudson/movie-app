f4d6ab10a657aca5a853bc32a9ee012cc1bb4077
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-02 10:01:13 +0000

    fix(test): add setup/cleanup steps to lifecycle concise state

--- api/tests/e2e/app.test.js
+++ api/tests/e2e/app.test.js
@@ -3,6 +3,7 @@ const request = require('supertest')
 
 const api = require('../../src/app')
 const { knex, migrate } = require('../../src/database')
+const { mockTmdbCall } = require('../integration/services/get-movies/adapters/helpers')
 
 beforeAll((done) => {
   migrate()
@@ -18,8 +19,27 @@ afterAll((done) => {
     })
 })
 
+beforeEach((done) => {
+  knex
+    .raw(`
+      alter sequence movie_id_seq restart with 1;
+      update movie set id = default;
+    `)
+    .then(() => {
+      knex('movie')
+        .del()
+        .finally(() => {
+          done()
+        })
+    })
+})
+
 describe('get /movies', () => {
   it('should return all movies from database', async () => {
+    await knex('movie').insert([
+      { id: 1, title: 'Black Panther', description: 'bp', director: 'bp', producer: 'bp', banner: 'bp', poster: 'bp', originalId: '1' },
+      { id: 2, title: 'Top Gun', description: 'tg', director: 'tg', producer: 'tg', banner: 'tg', poster: 'tg', originalId: '2' },
+    ])
     const { status, body } = await request(api)
       .get('/movies')
     expect(status).toBe(200)
@@ -28,43 +48,12 @@ describe('get /movies', () => {
 })
 
 describe('get /update', () => {
-  beforeEach((done) => {
-    knex
-      .raw(`
-        alter sequence movie_id_seq restart with 1;
-        update movie set id = default;
-      `)
-      .then(() => {
-        knex('movie')
-          .del()
-          .finally(() => {
-            done()
-          })
-      })
+  beforeEach(() => {
+    nock.cleanAll()
   })
 
   it('should retrieve data from ghibli api', async () => {
-    nock('https://ghibliapi.herokuapp.com/films')
-      .get('')
-      .reply(200, [
-        {
-          "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
-          "title": "Castle in the Sky",
-          "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
-          "director": "Hayao Miyazaki",
-          "producer": "Isao Takahata",
-          "image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
-          "movie_banner": "https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg",
-        },{
-          "id": "12cfb892-aac0-4c5b-94af-521852e46d6a",
-          "title": "Grave of the Fireflies",
-          "image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg",
-          "movie_banner": "https://image.tmdb.org/t/p/original/vkZSd0Lp8iCVBGpFH9L7LzLusjS.jpg",
-          "description": "In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.",
-          "director": "Isao Takahata",
-          "producer": "Toru Hara",
-        }
-      ]);
+    await mockTmdbCall()
 
     const { status, body } = await request(api)
       .get('/update')
