d17c21b43d836fea0ca08d430a4dde6cc1ea94ec
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-03 13:15:27 +0000

    refactor: move axios to an inverted dependency

---
+++ api/src/infra/http-request/adapters/axios.js
@@ -0,0 +1,5 @@
+const axios = require('axios')
+
+module.exports = ({ url, params }) => {
+  return axios.get(url, params)
+}
--- api/src/services/get-movies/adapters/tmdb.js
+++ api/src/services/get-movies/adapters/tmdb.js
@@ -1,5 +1,3 @@
-const axios = require('axios')
-
 const api_key = process.env.TMDB_API_KEY
 
 const filterReduce = (all) => {
@@ -15,14 +13,12 @@ const filterReduce = (all) => {
   })
 }
 
-const addDirectorAndProducer = async (movies) => {
+const addDirectorAndProducer = async (movies, doGetRequest) => {
   let resultMovies = []
   for (let movie of movies) {
-    const { data: credits } = await axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${movie.originalId}/credits`,
-        { api_key }
-      )
+    const url = `https://api.themoviedb.org/3/movie/${movie.originalId}/credits`
+    const params = { api_key }
+    const { data: credits } = await doGetRequest({ url, params })
     resultMovies.push({
       ...movie,
       director: credits.crew.filter(({ job }) => job === 'Director')
@@ -34,17 +30,22 @@ const addDirectorAndProducer = async (movies) => {
   return resultMovies
 }
 
-module.exports = async () => {
-  const { data: first } = await axios.get('https://api.themoviedb.org/4/list/1', { api_key })
+module.exports = async ({
+    doGetRequest = require('../../../infra/http-request/adapters/axios')
+  } = {}) => {
+  const url = 'https://api.themoviedb.org/4/list/1'
+  const params = { api_key }
+  const { data: first } = await doGetRequest({ url, params })
   let movies = [ ...first.results ]
   if (first.total_pages > 1) {
     for (let page = 2; page <= first.total_pages; page++) {
-      const { data: current } = await axios
-        .get('https://api.themoviedb.org/4/list/1', { api_key, page })
+      const { data: current } = await doGetRequest({
+        url, params: { ...params, page }
+      })
       movies = [ ...movies, ...current.results ]
     }
   }
   return Promise.resolve(
-    await addDirectorAndProducer([ ...filterReduce(movies) ])
+    await addDirectorAndProducer([ ...filterReduce(movies) ], doGetRequest)
   )
 }
