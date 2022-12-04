15a70d796fcf7b2484053bd6c793a336f368e21d
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-04 02:04:09 +0000

    fix(test): invert order of statement execution

--- api/tests/integration/repositories/helpers.js
+++ api/tests/integration/repositories/helpers.js
@@ -1,11 +1,11 @@
 const { knex } = require('../../../src/database')
 
 const clearMovieTable = async () => {
+  await knex('movie').del()
   await knex.raw(`
     alter sequence movie_id_seq restart with 1;
     update movie set id = default;
   `)
-  await knex('movie').del()
 }
 
 module.exports = { clearMovieTable }
