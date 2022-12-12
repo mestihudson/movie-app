1e8d0f1aba7f5f7423bb486df7923a96fdc758b1
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-12 09:18:15 +0000

    feat: add swagger docs router to api

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 const movieRouter = require('./routers/movie')
 
@@ -7,6 +9,30 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const specs = swaggerJsDoc({
+  failOnErrors: true,
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Movie-App API',
+      description: 'Serviços disponibilizados pela api da aplicação movie-app',
+      version: '1.0.0',
+    }
+  },
+  apis: ['../src/routers/movie.js'],
+})
+
+console.log(specs)
+
+app.use(
+  '/docs/',
+  async (req, res, next) => {
+    req.originalUrl = (req.headers['x-forwarded-prefix'] || '') + req.url
+    next()
+  },
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+)
 app.use('/', movieRouter)
 
 module.exports = app
