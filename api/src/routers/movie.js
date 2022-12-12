f421f6f3803e573b368f7310f172f04b6c9361f6
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-12 11:26:41 +0000

    docs: add swagger comments to swagger-jsdoc scan

--- api/src/routers/movie.js
+++ api/src/routers/movie.js
@@ -2,7 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
+/**
+ * @openapi
+ * /movies:
+ *   get:
+ *     description: Retrieve a page of all movies stored in local database
+ *     responses:
+ *       200:
+ *         description: success
+ */
 router.get('/movies', require('../controllers/retrieve-movies'))
+
+/**
+ * @openapi
+ * /update:
+ *   get:
+ *     description: Retrieve all movies from remote api and update database
+ *     responses:
+ *       200:
+ *         description: success
+ */
 router.get('/update', require('../controllers/update-base'))
 
 module.exports = router
