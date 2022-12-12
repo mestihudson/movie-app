07cfe07c3fb5c21de6beb419d8a4d578596eff07
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-12 12:32:15 +0000

    refactor: move schemas to specific file

--- api/src/app.js
+++ api/src/app.js
@@ -20,39 +20,7 @@ const specs = swaggerJsDoc({
     },
     components: {
       schemas: {
-        "RetrieveMoviesResponse": {
-          "type": "object",
-          "properties": {
-            "movies": {
-              "type": "array",
-              "items": {
-                "$ref": "#/components/schemas/Movie"
-              }
-            },
-            "total": {
-              "type": "number"
-            }
-          }
-        },
-        "Movie": {
-          "type": "object",
-          "properties": {
-            "id": { "type": "number" },
-            "title": { "type": "string" },
-            "description": { "type": "string" },
-            "director": { "type": "string" },
-            "producer": { "type": "string" },
-            "banner": { "type": "string" },
-            "poster": { "type": "string" },
-            "originalId": { "type": "string" }
-          }
-        },
-        "UpdateBaseResponse": {
-          "type": "object",
-          "properties": {
-            "modified": { "type": "boolean" }
-          }
-        }
+        ...require('./schemas-swagger.json')
       },
     },
   },
