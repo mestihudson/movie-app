a6a9ecee47e783ac15c00156be2389568f105157
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-21 14:38:18 +0000

    chore: add react app entry point

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App'
+
+import './index.css'
+
+createRoot(document.querySelector('#root'))
+  .render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
