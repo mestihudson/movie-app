b72c72e9f41e41c9fc00cd0b5ee84ac6238ab6b4
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-22 01:53:01 +0000

    chore: copy integration test from legacy to new project structure

---
+++ client/tests/integration/services/RetrieveMoviesService.test.js
@@ -0,0 +1,28 @@
+import { retrieveMoviesService } from '../../../src/services/RetrieveMoviesService'
+import { rest, server } from '../mocks/server'
+
+it('should handler successful api response', async () => {
+  server.use(
+    rest.get('/api/movies', (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([1,2])
+      )
+    })
+  )
+
+  await expect(retrieveMoviesService()).resolves.toStrictEqual([1,2])
+})
+
+it('should handler error api response', async () => {
+  server.use(
+    rest.get('/api/movies', (req, res, ctx) => {
+      return res(
+        ctx.status(500),
+        ctx.json({})
+      )
+    })
+  )
+
+  await expect(retrieveMoviesService()).rejects.toThrow()
+})
---
+++ client/tests/integration/services/UpdateBaseService.test.js
@@ -0,0 +1,28 @@
+import { updateBaseService } from '../../../src/services/UpdateBaseService'
+import { rest, server } from '../mocks/server'
+
+it('should handler successful api response', async () => {
+  server.use(
+    rest.get('/api/update', (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({})
+      )
+    })
+  )
+
+  await expect(updateBaseService()).resolves.toStrictEqual({})
+})
+
+it('should handler error api response', async () => {
+  server.use(
+    rest.get('/api/update', (req, res, ctx) => {
+      return res(
+        ctx.status(500),
+        ctx.json({})
+      )
+    })
+  )
+
+  await expect(updateBaseService()).rejects.toThrow()
+})
