4d73c77b584330f5def9167c2f4ddb9986470cbc
Hudson Ferreira Leite <mestihudson@gmail.com>
2022-12-29 01:16:27 +0000

    style: add specific file to app component

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import MovieListPage from './components/MovieListPage'
 import { updateBaseService } from './services/UpdateBaseService'
 import { retrieveMoviesService } from './services/RetrieveMoviesService'
 
+import './App.css'
+
 export default function App() {
   return (
     <>
