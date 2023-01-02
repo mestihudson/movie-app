4e1aef4b0517f2fd82617c1348ba5fbdd71e653d
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-01-02 19:07:03 +0000

    test(unit/todo): pagination

---
+++ app/tests/unit/components/Pagination.test.js
@@ -0,0 +1,8 @@
+it.todo("should render pagination when total > limit")
+it.todo("should not render pagination when total <= limit")
+it.todo("should render '%s' page buttons group when total = '%' and limit = '%s'")
+it.todo("should render '%s' label to '%s' button when it has customized")
+it.todo("should render '%s' disabled when page count is '%s' and current = '%s'")
+it.todo("should not be possible to click a disabled button")
+it.todo("should trigger go to page when page button is clicked")
+it.todo("should go to page '%s' when '%s' page button is clicked")
