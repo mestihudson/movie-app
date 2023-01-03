3b7a93392fbf1ebc560b9139ed81eb9a6fbac6cf
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-01-03 01:53:16 +0000

    feat: render %s page buttons when total = %s and limit = %s

--- app/src/components/Pagination.js
+++ app/src/components/Pagination.js
@@ -1,8 +1,23 @@
 export default function Pagination({ total, limit }) {
+  const pages = new Array(Math.ceil(total / limit))
+    .fill(0)
+    .map((e, i) => {
+      const key = i + 1
+      const label = key
+      return { key, label }
+    })
+  const items = [
+    { key: 'prev', label: 'P' },
+    ...pages,
+    { key: 'next', label: 'N' },
+  ]
   return (
     <>
     { total > limit && (
       <div data-testid='pagination'>
+        { items.map(({ key, label }) =>
+          <li key={key} data-testid='page-item'>{label}</li>
+        )}
       </div>
     )}
     </>
