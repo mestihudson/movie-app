5e4b91574553bc489b58e1cbad00f84b0e657c68
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-01-13 14:32:13 +0000

    chore: add @cypress/code-coverage configs

--- app/tests/e2e/cypress.config.js
+++ app/tests/e2e/cypress.config.js
@@ -3,9 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.BASE_URL || 'http://localhost:14000',
-    supportFile: false,
+    supportFile: 'tests/e2e/support/index.js',
     specPattern: 'tests/e2e/specs/**/*.cy.js',
     videosFolder: 'tests/e2e/videos',
     screenshotsFolder: 'tests/e2e/screenshots',
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    }
   },
 })
