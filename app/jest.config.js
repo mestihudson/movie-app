a727a9c326eb5eb70f21d576b49b1757125d33a2
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-01-17 18:07:47 +0000

    chore: add html among coverage reporters

--- app/jest.config.js
+++ app/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['<rootDir>/src/**/*.js',],
   collectCoverage: true,
-  coverageReporters: ['json'],
+  coverageReporters: ['json', 'html'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
     "^@/(.*)$": "<rootDir>/src/$1",
